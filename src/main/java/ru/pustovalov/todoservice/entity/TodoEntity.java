91a3b9155f926ca013931a34a75fe637d4e468d5
Semyon <semyon.pustovalov@gmail.com>
2025-07-22 16:06:45 +0300

    initial entity

---
+++ src/main/java/ru/pustovalov/todoservice/entity/TodoEntity.java
@@ -0,0 +1,48 @@
+package ru.pustovalov.todoservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+@Table(name = "todo", schema = "public")
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "action", nullable = false)
+    private String action;
+
+    @Column(name = "action_timestamp", nullable = false)
+    private Timestamp actionTimestamp;
+
+    @Column(name = "editor", nullable = false)
+    private String editor;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        TodoEntity that = (TodoEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
